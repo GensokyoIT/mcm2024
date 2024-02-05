e1c5ea4505e83ae93e6f3f56bb0db6526b54554d
TonyZhang <mcerztn@gmail.com>
2024-02-06 06:40:52 +0800

    update tex

--- Src/test.py
+++ Src/test.py
@@ -6,6 +6,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Energy needed by a single fish
+for i in range(6):
+    for j in range (6):
+        print("A_{%d%d} &"%(i+1,j+1),end="")
+    print("\\\\")
 x = np.linspace(0,20,101)
 y= 0.12+x
 # Set labels and title
@@ -19,4 +23,4 @@ plt.ylim(0,1.2)
 plt.yticks([])
 plt.xlim(0,1)
 # Show the plot
-plt.show()
+#plt.show()
